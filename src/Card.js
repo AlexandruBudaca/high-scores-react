8496fa0351cd650c2e5c3fa92c64f70eb94ecb7b
Alex <alex.andrul10@gmail.com>
2020-05-25 20:53:08 +0100

    lvl < 4

--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Card = ({ name, scores }) => {
-  scores.sort((a, b) => (a.s > b.s ? -1 : 1));
+  const [toggle, setToggle] = useState(
+    scores.sort((a, b) => (a.s > b.s ? -1 : 1))
+  );
+
+  const switchScores = () => {
+    const newToggle = [...toggle].sort((a, b) => (a.s < b.s ? -1 : 1));
+    setToggle(newToggle);
+  };
+
   return (
-    <div className="card-country">
-      <h2>HIGH SCORES: {name}</h2>
-      <div className="card-details">
-        {scores.map((score, index) => (
-          <div key={index} className="card-scores">
-            <div className="score-name">
-              <h3>{score.n.toUpperCase()}</h3>
+    <div>
+      <div className="card-country">
+        <h2>HIGH SCORES: {name}</h2>
+        <button onClick={switchScores}>Ascending</button>
+        <div className="card-details">
+          {toggle.map((score, index) => (
+            <div key={index} className="card-scores">
+              <div className="score-name">
+                <h3>{score.n.toUpperCase()}</h3>
+              </div>
+              <div className="score">
+                <h3>{score.s}</h3>
+              </div>
             </div>
-            <div className="score">
-              <h3>{score.s}</h3>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
